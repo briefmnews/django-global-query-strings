4241792df53b1e79a8b9bf9e4bcf4a6e48d5138f
Damien Cirotteau <d@cirotteau.info>
2020-12-09 16:03:29 +0100

    fix(middleware): use only on text/html

--- global_query_strings/middleware.py
+++ global_query_strings/middleware.py
@@ -10,12 +10,9 @@ class GlobalQueryStringsMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
 
-        # Handle image in Content-Type response
-        # and handle StreamingHttpResponse response such as large CSV or SVG files.
         if (
-            "Content-Type" in response and "image" in response["Content-Type"]
-        ) or isinstance(response, StreamingHttpResponse):
-            return response
+            "Content-Type" in response and "text/html" in response["Content-Type"]
+        ) :
+            response.content = add_query_strings_to_links(response.content)
 
-        response.content = add_query_strings_to_links(response.content)
         return response
